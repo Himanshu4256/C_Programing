eca2f8cf31e27ec531e13aa132631c0b8f2ae6f5
Himanshu sharma <111674774+Himanshu4256@users.noreply.github.com>
2023-07-26 12:00:19 +0530

    Add files via upload
    initial commit

---
+++ main5.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  logic building
+//
+
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k;
+    for (i=0;i<=5;i++)
+    {
+        for(k=5;k>=1;i--)
+        printf(" ");
+        for(j=0;j<=5;j++)
+        printf(" *");
+    }
+}
---
+++ mains.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  Pattern1
+//
+//  Created by Himanshu Sharma on 26/05/21.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    
+    for(i=1;i<=4;i++)
+    {
+    for(j=1;j<=i;j++)
+        {
+        printf("* ");
+                }
+            printf("\n");
+    }
+
+    
+}
+
+
